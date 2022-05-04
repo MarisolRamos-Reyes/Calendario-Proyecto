dafe033b70ebc301e1a0c2437fe932858fdf834d
MarisolRamos <71091704+MarisolRamos-Reyes@users.noreply.github.com>
2022-05-03 21:36:21 -0500

    Add files via upload
    En este programa esta creado un calendario el cual nos ayudara a organizar eventos del Tec Monclova.

---
+++ Calendario-Proyecto/Calendario-Proyecto/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Calendario_Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textcontraseña = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btnIngresar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calendario_Proyecto.Properties.Resources.logo_tec;
+            this.pictureBox1.Location = new System.Drawing.Point(73, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textcontraseña
+            // 
+            this.textcontraseña.Depth = 0;
+            this.textcontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcontraseña.ForeColor = System.Drawing.Color.White;
+            this.textcontraseña.Hint = "Contraseña ";
+            this.textcontraseña.Location = new System.Drawing.Point(57, 254);
+            this.textcontraseña.MouseState = MaterialSkin.MouseState.HOVER;
+            this.textcontraseña.Name = "textcontraseña";
+            this.textcontraseña.PasswordChar = '*';
+            this.textcontraseña.SelectedText = "";
+            this.textcontraseña.SelectionLength = 0;
+            this.textcontraseña.SelectionStart = 0;
+            this.textcontraseña.Size = new System.Drawing.Size(271, 23);
+            this.textcontraseña.TabIndex = 11;
+            this.textcontraseña.UseSystemPasswordChar = false;
+            
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Depth = 0;
+            this.btnIngresar.Location = new System.Drawing.Point(109, 307);
+            this.btnIngresar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Primary = true;
+            this.btnIngresar.Size = new System.Drawing.Size(168, 35);
+            this.btnIngresar.TabIndex = 12;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(145)))), ((int)(((byte)(243)))));
+            this.label1.Location = new System.Drawing.Point(27, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(27, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "label2";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Director",
+            "Personal Academico",
+            "Personal de Planeación",
+            "Personal Financiero"});
+            this.comboBox1.Location = new System.Drawing.Point(57, 213);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 26);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.Tag = "";
+            this.comboBox1.Text = "Usuario";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 427);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.textcontraseña);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Iniciar Sesión ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField textcontraseña;
+        private MaterialSkin.Controls.MaterialRaisedButton btnIngresar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Calendario-Proyecto/Calendario-Proyecto/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace Calendario_Proyecto
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var skingmanager = MaterialSkinManager.Instance;
+            skingmanager.AddFormToManage(this);
+            skingmanager.Theme = MaterialSkinManager.Themes.DARK;
+            skingmanager.ColorScheme = new ColorScheme(Primary.Blue900, Primary.Blue700, Primary.Blue700,
+                Accent.Blue400, TextShade.WHITE);
+            //Labelfecha.Text = DateTime.Now.ToLongDateString();
+            //labelhora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox1.Text == "Director" && this.textcontraseña.Text == "123456789" || this.comboBox1.Text == "Personal Academico" && this.textcontraseña.Text == "123"
+                || this.comboBox1.Text == "Personal de Planeación" && this.textcontraseña.Text == "456" || this.comboBox1.Text == "Personal Financiero" && this.textcontraseña.Text == "789")
+            {
+                Form Formulario = new Calendario();
+                Formulario.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Revise su contraseña, debe de estar mal escrita", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+        }
+
+        
+    }
+}
